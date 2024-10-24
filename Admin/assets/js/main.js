dba98ebb208da7529cc078181e1fa0f3851bd66d
nurrr357 <muhamadhuda@gmail.com>
2024-10-24 19:23:27 +0700

    add modal

--- Admin/assets/js/main.js
+++ Admin/assets/js/main.js
@@ -19,3 +19,36 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+var modal = document.getElementById('myModal');
+
+var openModalBtn = document.getElementById('openModalBtn');
+
+var closeSpan = document.getElementsByClassName('close')[0];
+
+var closeModalBtn = document.getElementById('closeModalBtn');
+
+openModalBtn.onclick = function() {
+    modal.style.display = 'block';
+    function showMessage() {
+      alert("Ini adalah pesan yang ditampilkan setelah 3 detik!");
+    }
+    
+    setTimeout(showMessage, 3000);
+}
+
+closeSpan.onclick = function() {
+    modal.style.display = 'none';
+}
+
+closeModalBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
